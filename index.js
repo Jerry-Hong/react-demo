61db9252d8b5dbbab5a1b4c29e08d385056a80d6
JerryHong <s6323859@hotmail.com>
2019-01-29 11:28:42 +0800

    complete

---
+++ index.js
@@ -0,0 +1,37 @@
+var newCourseList = [
+	{
+		"id": 511021,
+		"title": "React for Beginners",
+		"coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
+		"rating": 5
+	},
+	{
+		"id": 511022,
+		"title": "Vue2 for Beginners",
+		"coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
+		"rating": 5
+	},
+	{
+		"id": 511023,
+		"title": "Angular2 for Beginners",
+		"coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
+		"rating": 5
+	},
+	{
+		"id": 511024,
+		"title": "Webpack for Beginners",
+		"coverPng": "https://res.cloudinary.com/dohtkyi84/image/upload/v1481226146/react-cover.png",
+		"rating": 4
+	}
+], idAndTitle = [];
+
+
+/* idAndTitle => [
+    {id:511021, "title": "React for Beginners",}, 
+    {"id": 511022,"title": "Vue2 for Beginners",},
+    {"id": 511023, "title": "Angular2 for Beginners",}, 
+    {"id": 511024, "title": "Webpack for Beginners",}
+]
+*/
+
+console.log(idAndTitle)
